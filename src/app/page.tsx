ff5fcb25bb4d074177421a7633fdd5dff48cbac9
J-J9M1 <73731892+Jima-J9M1@users.noreply.github.com>
2026-06-14 15:41:31 +0300

    Refactor Home component and clean up imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,37 +6,35 @@ import { Contact } from "@portfolio/components/sections/contact";
 import { Experience } from "@portfolio/components/sections/Experience";
 import { Footer } from "@portfolio/components/sections/Footer";
 import { Hero } from "@portfolio/components/sections/Hero";
-import { Projects } from "@portfolio/components/sections/project";
+import Projects from "@portfolio/components/sections/project";
 import { Skills } from "@portfolio/components/sections/Skills";
 import { useTheme } from "next-themes";
 
 export default function Home() {
-  // access theme value from context
-  const { theme } = useTheme();
-  console.log(theme);
-  return (
-    <main className="min-h-screen">
-      <Waves
-        lineColor= {theme === "dark" ? "white" : "black"}
-        backgroundColor="rgba(255, 255, 255, 0.2)"
-        waveSpeedX={0.02}
-        waveSpeedY={0.01}
-        waveAmpX={40}
-        waveAmpY={20}
-        friction={0.9}
-        tension={0.01}
-        maxCursorMove={120}
-        xGap={12}
-        yGap={36}
-      />
-      <Header />
-      <Hero />
-      <About />
-      <Projects />
-      <Skills />
-      <Experience />
-      <Contact />
-      <Footer />
-    </main>
-  )
-}
+    const { theme } = useTheme();
+    return (
+          <main className="min-h-screen">
+                <Waves
+                          lineColor={theme === "dark" ? "white" : "black"}
+                          backgroundColor="rgba(255, 255, 255, 0.2)"
+                          waveSpeedX={0.02}
+                          waveSpeedY={0.01}
+                          waveAmpX={40}
+                          waveAmpY={20}
+                          friction={0.9}
+                          tension={0.01}
+                          maxCursorMove={120}
+                          xGap={12}
+                          yGap={36}
+                        />
+                <Header />
+                <Hero />
+                <About />
+                <Skills />
+                <Projects />
+                <Experience />
+                <Contact />
+                <Footer />
+          </main>main>
+        );
+}</main>
